8ab39c5b34976983c72ae8c287baf15ed0f38615
Skriptoon <pavel.deev45@gmail.com>
2020-09-01 17:41:22 +0500

    1

--- src/inventory.js
+++ src/inventory.js
@@ -2,19 +2,22 @@
 
 const size_x = 5;
 const size_y = 5;
-$("#root").append('<div class="inventory">\
+
+var body = '<div class="inventory">\
 <div class="head">\
     <h4>Инвентарь</h4>\
 </div>\
 <div class="cells">\
-    <table class="table-bordered">');
-for(let i = 0; i < size_x; i++) {
-  $("#root").append('<tr>');
-  for(var k = 0; k < size_y; k++) {
-    $("#root").append('<td class="cell"><div class="cell-body"></div></td>');
+    <table class="table-bordered">';
+for (var i = 0; i < size_x; i++) {
+  body += '<tr>';
+  for (var k = 0; k < size_y; k++) {
+    body += '<td class="cell"><div class="cell-body" id="' + (i * 5 + k) + '"></div></td>';
   }
-  $("#root").append('</tr>');
+  body += '</tr>';
 }
+
+$("#root").append(body);
 /*
 class Inventory extends React.Component {
 
