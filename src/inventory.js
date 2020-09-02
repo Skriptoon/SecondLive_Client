1d08e4081df6b46a9b3e2fcf89bfb6ce5b55fef6
Skriptoon <pavel.deev45@gmail.com>
2020-09-02 18:37:15 +0500

    1

--- src/inventory.js
+++ src/inventory.js
@@ -14,26 +14,56 @@ class Cells {
 }
 
 class Item {
-  constructor(cell, x, y) {
+  constructor(cell, x, y, type) {
     this.cell = cell;
     this.x = x;
     this.y = y;
+    this.type = type;
   }
 }
 
 class Inventory extends React.Component {
   render() {
     return(
-      <div className="inventory">
-      <div className="head">
-          <h4>Инвентарь</h4>
-      </div>
-        <div className="cells">
-          <table className="table-bordered">
-            <tbody>
-              {RenderCell(size_x, size_y)}
-            </tbody>
-          </table>
+      <div className="inventory row">
+        <div className="col-10">
+          <div className="row">
+            <div className="equip col-auto">
+              <div className="head">
+                <h4>Экипировка</h4>
+              </div>
+              <table>
+                <tbody>
+                  <tr>
+                    <td><div className="equip-cell" id="hat"></div></td>
+                    <td><div className="equip-cell" id="mask"></div></td>
+                    <td><div className="equip-cell" id="glass"></div></td>
+                    <td><div className="equip-cell" id="ears"></div></td>
+                    <td><div className="equip-cell" id="top"></div></td>
+                    <td><div className="equip-cell" id="leg"></div></td>
+                  </tr>
+                  <tr>
+                    <td><div className="equip-cell" id="shoes"></div></td>
+                    <td><div className="equip-cell" id="accessories"></div></td>
+                    <td><div className="equip-cell" id="armor"></div></td>
+                    <td><div className="equip-cell" id="watche"></div></td>
+                    <td><div className="equip-cell" id="bracelet"></div></td>
+                    <td><div className="equip-cell" id="backpack"></div></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div className="cells col-auto">
+              <div className="head">
+                <h4>Инвентарь</h4>
+              </div>
+              <table className="table-bordered">
+                <tbody>
+                  {RenderCell(size_x, size_y)}
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
     )
@@ -68,7 +98,20 @@ $( ".cell-body" ).droppable({
         setTimeout(() => {
         cell = false;
         }, 100)
-        
+
+        if($(ui.draggable).attr("data-size-x") == 1 && $(ui.draggable).attr("data-size-x") == 1) {
+          console.log();
+          if(drop_coords.top == drag_coords.top) {
+            if((drop_coords.left + 30 - drag_coords.left) * 30 / (30*30) > 0.5)
+            push_cell(ui.draggable, this);
+            return;
+          }
+          if(drop_coords.left == drag_coords.left) {
+            if((drop_coords.top + 30 - drag_coords.top) * 30 / (30*30) > 0.5)
+            push_cell(ui.draggable, this);
+            return;
+          }
+        }
         if((drop_coords.top < drag_coords.top && drag_coords.top < (drop_coords.top + 30))
         && (drop_coords.left < drag_coords.left && drag_coords.left < (drop_coords.left + 30))) {
             if((drop_coords.top + 30 - drag_coords.top) * (drop_coords.left + 30 - drag_coords.left) / (30*30) > 0.3) {
@@ -103,6 +146,20 @@ $( ".cell-body" ).droppable({
     },
 
     tolerance: "touch"
+});
+
+$(".equip").droppable({
+  drop: function(event, ui) {
+    if(cell) return;
+
+    setTimeout(() => {
+    cell = false;
+    }, 100);
+
+    cell = true;
+
+
+  }
 });
 
 function push_cell(drag, drop) {
@@ -140,21 +197,31 @@ function check_cells(id, drop) {
   return true;
 }
 
-function add_item(x, y, img) {
+function add_item(x, y, img, type = "item") {
   $("#root").append('<div class="obj" id="item-' + item + '" data-size-x="' + x + '" data-size-y="' + y + '"><img src="' + img + '" width="100%"></div>');
   var szcell = get_freecell(x, y);
   if(szcell == -1) {
     alert("Нет места");
     return;
   }
-  $("#item-" + item).css("width", x * 64 + x - 1)
-    .css("height", y * 64 + y - 1)
+  $("#item-" + item).css("width", x * 30 + x - 1)
+    .css("height", y * 30 + y - 1)
     .draggable({
     start: function( event, ui ) {
       //pos = $(this).offset();
       for(var i = 0; i < $(this).attr("data-size-x"); i++) {
         for(var k = 0; k < $(this).attr("data-size-y"); k++) {
           cells[items[Number($(this).attr("id").substr(5))].cell + i + k * size_x].active = false;
+        }
+      }
+    },
+    stop: function(event, ui) {
+      if(!cell) {
+        $(this).offset($("#" + items[Number($(this).attr("id").substr(5))].cell).offset());
+        for(var i = 0; i < $(this).attr("data-size-x"); i++) {
+          for(var k = 0; k < $(this).attr("data-size-y"); k++) {
+            cells[items[Number($(this).attr("id").substr(5))].cell + i + k * size_x].active = true;
+          }
         }
       }
     }
@@ -165,7 +232,7 @@ function add_item(x, y, img) {
       cells[szcell + i + k * size_x].active = true;
     }
   }
-  items[item] = new Item(szcell, x, y);
+  items[item] = new Item(szcell, x, y, type);
   item++;
 }
 
--- ui/inventory.js
+++ ui/inventory.js
@@ -21,12 +21,13 @@ var Cells = function Cells() {
   this.active = false;
 };
 
-var Item = function Item(cell, x, y) {
+var Item = function Item(cell, x, y, type) {
   _classCallCheck(this, Item);
 
   this.cell = cell;
   this.x = x;
   this.y = y;
+  this.type = type;
 };
 
 var Inventory = function (_React$Component) {
@@ -43,26 +44,123 @@ var Inventory = function (_React$Component) {
     value: function render() {
       return React.createElement(
         "div",
-        { className: "inventory" },
+        { className: "inventory row" },
         React.createElement(
           "div",
-          { className: "head" },
+          { className: "col-10" },
           React.createElement(
-            "h4",
-            null,
-            "\u0418\u043D\u0432\u0435\u043D\u0442\u0430\u0440\u044C"
-          )
-        ),
-        React.createElement(
-          "div",
-          { className: "cells" },
-          React.createElement(
-            "table",
-            { className: "table-bordered" },
+            "div",
+            { className: "row" },
             React.createElement(
-              "tbody",
-              null,
-              RenderCell(size_x, size_y)
+              "div",
+              { className: "equip col-auto" },
+              React.createElement(
+                "div",
+                { className: "head" },
+                React.createElement(
+                  "h4",
+                  null,
+                  "\u042D\u043A\u0438\u043F\u0438\u0440\u043E\u0432\u043A\u0430"
+                )
+              ),
+              React.createElement(
+                "table",
+                null,
+                React.createElement(
+                  "tbody",
+                  null,
+                  React.createElement(
+                    "tr",
+                    null,
+                    React.createElement(
+                      "td",
+                      null,
+                      React.createElement("div", { className: "equip-cell", id: "hat" })
+                    ),
+                    React.createElement(
+                      "td",
+                      null,
+                      React.createElement("div", { className: "equip-cell", id: "mask" })
+                    ),
+                    React.createElement(
+                      "td",
+                      null,
+                      React.createElement("div", { className: "equip-cell", id: "glass" })
+                    ),
+                    React.createElement(
+                      "td",
+                      null,
+                      React.createElement("div", { className: "equip-cell", id: "ears" })
+                    ),
+                    React.createElement(
+                      "td",
+                      null,
+                      React.createElement("div", { className: "equip-cell", id: "top" })
+                    ),
+                    React.createElement(
+                      "td",
+                      null,
+                      React.createElement("div", { className: "equip-cell", id: "leg" })
+                    )
+                  ),
+                  React.createElement(
+                    "tr",
+                    null,
+                    React.createElement(
+                      "td",
+                      null,
+                      React.createElement("div", { className: "equip-cell", id: "shoes" })
+                    ),
+                    React.createElement(
+                      "td",
+                      null,
+                      React.createElement("div", { className: "equip-cell", id: "accessories" })
+                    ),
+                    React.createElement(
+                      "td",
+                      null,
+                      React.createElement("div", { className: "equip-cell", id: "armor" })
+                    ),
+                    React.createElement(
+                      "td",
+                      null,
+                      React.createElement("div", { className: "equip-cell", id: "watche" })
+                    ),
+                    React.createElement(
+                      "td",
+                      null,
+                      React.createElement("div", { className: "equip-cell", id: "bracelet" })
+                    ),
+                    React.createElement(
+                      "td",
+                      null,
+                      React.createElement("div", { className: "equip-cell", id: "backpack" })
+                    )
+                  )
+                )
+              )
+            ),
+            React.createElement(
+              "div",
+              { className: "cells col-auto" },
+              React.createElement(
+                "div",
+                { className: "head" },
+                React.createElement(
+                  "h4",
+                  null,
+                  "\u0418\u043D\u0432\u0435\u043D\u0442\u0430\u0440\u044C"
+                )
+              ),
+              React.createElement(
+                "table",
+                { className: "table-bordered" },
+                React.createElement(
+                  "tbody",
+                  null,
+                  RenderCell(size_x, size_y)
+                )
+              )
             )
           )
         )
@@ -110,6 +208,17 @@ $(".cell-body").droppable({
       cell = false;
     }, 100);
 
+    if ($(ui.draggable).attr("data-size-x") == 1 && $(ui.draggable).attr("data-size-x") == 1) {
+      console.log();
+      if (drop_coords.top == drag_coords.top) {
+        if ((drop_coords.left + 30 - drag_coords.left) * 30 / (30 * 30) > 0.5) push_cell(ui.draggable, this);
+        return;
+      }
+      if (drop_coords.left == drag_coords.left) {
+        if ((drop_coords.top + 30 - drag_coords.top) * 30 / (30 * 30) > 0.5) push_cell(ui.draggable, this);
+        return;
+      }
+    }
     if (drop_coords.top < drag_coords.top && drag_coords.top < drop_coords.top + 30 && drop_coords.left < drag_coords.left && drag_coords.left < drop_coords.left + 30) {
       if ((drop_coords.top + 30 - drag_coords.top) * (drop_coords.left + 30 - drag_coords.left) / (30 * 30) > 0.3) {
         push_cell(ui.draggable, this);
@@ -140,6 +249,18 @@ $(".cell-body").droppable({
   },
 
   tolerance: "touch"
+});
+
+$(".equip").droppable({
+  drop: function drop(event, ui) {
+    if (cell) return;
+
+    setTimeout(function () {
+      cell = false;
+    }, 100);
+
+    cell = true;
+  }
 });
 
 function push_cell(drag, drop) {
@@ -174,18 +295,30 @@ function check_cells(id, drop) {
 }
 
 function add_item(x, y, img) {
+  var type = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : "item";
+
   $("#root").append('<div class="obj" id="item-' + item + '" data-size-x="' + x + '" data-size-y="' + y + '"><img src="' + img + '" width="100%"></div>');
   var szcell = get_freecell(x, y);
   if (szcell == -1) {
     alert("Нет места");
     return;
   }
-  $("#item-" + item).css("width", x * 64 + x - 1).css("height", y * 64 + y - 1).draggable({
+  $("#item-" + item).css("width", x * 30 + x - 1).css("height", y * 30 + y - 1).draggable({
     start: function start(event, ui) {
       //pos = $(this).offset();
       for (var i = 0; i < $(this).attr("data-size-x"); i++) {
         for (var k = 0; k < $(this).attr("data-size-y"); k++) {
           cells[items[Number($(this).attr("id").substr(5))].cell + i + k * size_x].active = false;
+        }
+      }
+    },
+    stop: function stop(event, ui) {
+      if (!cell) {
+        $(this).offset($("#" + items[Number($(this).attr("id").substr(5))].cell).offset());
+        for (var i = 0; i < $(this).attr("data-size-x"); i++) {
+          for (var k = 0; k < $(this).attr("data-size-y"); k++) {
+            cells[items[Number($(this).attr("id").substr(5))].cell + i + k * size_x].active = true;
+          }
         }
       }
     }
@@ -195,7 +328,7 @@ function add_item(x, y, img) {
       cells[szcell + i + k * size_x].active = true;
     }
   }
-  items[item] = new Item(szcell, x, y);
+  items[item] = new Item(szcell, x, y, type);
   item++;
 }
 
